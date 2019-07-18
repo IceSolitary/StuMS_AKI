fdad9f7aa72079b4baec5fbdc3af3c26bd340a26
iceSolitary <2457732990@qq.com>
2019-07-18 16:16:26 +0800

    tool man part part push

--- StuMS_AKI/src/cn/edu/cuc/aki/stuMS/tools/MySQLConnector.java
+++ StuMS_AKI/src/cn/edu/cuc/aki/stuMS/tools/MySQLConnector.java
@@ -7,93 +7,92 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class MySQLConnector {
-	 static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";  
-	    static final String DB_URL = "jdbc:mysql://localhost:3306/stuinfomanage?useSSL=false&serverTimezone=UTC";
-	 
-	 
-	    // 数据库的用户名与密码，需要根据自己的设置
-	    static final String USER = "root";
-	    static final String PASS = "Heartcheerful";
-	    static Connection conn = null;
-        static Statement stmt = null;
-	    
-        
-        // 完成后关闭
-        
-      
-	    public static ResultSet returnConnect(String sql) {
-	    	 
-	         try{
-	             // 注册 JDBC 驱动
-	             Class.forName(JDBC_DRIVER);
-	         
-	             // 打开链接
-	             System.out.println("连接数据库...");
-	             conn = DriverManager.getConnection(DB_URL,USER,PASS);
-	         
-	             // 执行查询
-	             System.out.println(" 实例化Statement对象...");
-	             stmt = conn.createStatement();
-	             
-	             ResultSet rs = stmt.executeQuery(sql);
-	         
-	           
-	             return rs;
-	             
-	         }catch(SQLException se){
-	             // 处理 JDBC 错误
-	             se.printStackTrace();
-	             return null;
-	         }catch(Exception e){
-	             // 处理 Class.forName 错误
-	             e.printStackTrace();
-	             return null;
-	         }
-	    }
-	    
 
-	    public static void connect(String sql) {
-	    	 
-	         try{
-	             // 注册 JDBC 驱动
-	             Class.forName(JDBC_DRIVER);
-	         
-	             // 打开链接
-	             System.out.println("连接数据库...");
-	             conn = DriverManager.getConnection(DB_URL,USER,PASS);
-	         
-	             // 执行查询
-	             System.out.println(" 实例化Statement对象...");
-	             stmt = conn.createStatement();
-	             
-	             stmt.execute(sql);
+	/**
+	 * mysql连接工具
+	 */
+	static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://localhost:3306/stuinfomanage?useSSL=false&serverTimezone=UTC";
 
-	         }catch(SQLException se){
-	             // 处理 JDBC 错误
-	             se.printStackTrace();
-	             
-	         }catch(Exception e){
-	             // 处理 Class.forName 错误
-	             e.printStackTrace();
-	            
-	         }
-	    }
-	    
-	    public static void disconnect() {
-	    	try {
-	    		  
-	             // 完成后关闭
-	    		if(stmt!=null) stmt.close();
-	    		
-	    	}catch (SQLException e) {
-				// TODO: handle exception
-			}
-	    	try {
-	    		if(conn!=null) conn.close();
-	    	}catch (SQLException se) {
-				// TODO: handle exception
-	    		se.printStackTrace();
-			}
-	    }
-	  
+	// 数据库的用户名与密码，需要根据自己的设置
+	static final String USER = "root";
+	static final String PASS = "Heartcheerful";
+	static Connection conn = null;
+	static Statement stmt = null;
+
+	// 完成后关闭
+
+	public static ResultSet returnConnect(String sql) {
+
+		try {
+			// 注册 JDBC 驱动
+			Class.forName(JDBC_DRIVER);
+
+			// 打开链接
+			System.out.println("连接数据库...");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+			// 执行查询
+			System.out.println(" 实例化Statement对象...");
+			stmt = conn.createStatement();
+
+			ResultSet rs = stmt.executeQuery(sql);
+
+			return rs;
+
+		} catch (SQLException se) {
+			// 处理 JDBC 错误
+			se.printStackTrace();
+			return null;
+		} catch (Exception e) {
+			// 处理 Class.forName 错误
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static void connect(String sql) {
+
+		try {
+			// 注册 JDBC 驱动
+			Class.forName(JDBC_DRIVER);
+
+			// 打开链接
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+			// 执行查询
+			stmt = conn.createStatement();
+
+			stmt.execute(sql);
+
+		} catch (SQLException se) {
+			// 处理 JDBC 错误
+			se.printStackTrace();
+
+		} catch (Exception e) {
+			// 处理 Class.forName 错误
+			e.printStackTrace();
+
+		}
+	}
+
+	public static void disconnect() {
+		try {
+
+			// 完成后关闭
+			if (stmt != null)
+				stmt.close();
+
+		} catch (SQLException e) {
+			// TODO: handle exception
+		}
+		try {
+			if (conn != null)
+				conn.close();
+		} catch (SQLException se) {
+			// TODO: handle exception
+			se.printStackTrace();
+		}
+	}
+
 }
--- StuMS_AKI/src/cn/edu/cuc/aki/stuMS/tools/STeaTools.java
+++ StuMS_AKI/src/cn/edu/cuc/aki/stuMS/tools/STeaTools.java
@@ -2,17 +2,35 @@ package cn.edu.cuc.aki.stuMS.tools;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import cn.edu.cuc.aki.stuMS.exception.CourseNotExistException;
+import cn.edu.cuc.aki.stuMS.exception.CourseNotMatchTeacherException;
+import cn.edu.cuc.aki.stuMS.exception.StudentNotExistException;
+import cn.edu.cuc.aki.stuMS.exception.TeacherNotExistException;
+import cn.edu.cuc.aki.stuMS.tools.log.LogIplm;
+import cn.edu.cuc.aki.stuMS.tools.log.LogIplm.TYPE;
+
 public class STeaTools {
 	
-	public static Map<String,String> selectStuInfo(String tid) { 
-		String sql = "select * from steacher where tid = '" + tid +"';";
+	/**
+	 * 教务处老师方法
+	 */
+
+	/**
+	 * 查询教务处老师的个人信息
+	 * 
+	 * @param tid 教务处老师的id
+	 * @return 教务处老师的个人信息字典
+	 */
+	public static Map<String, String> selectSTInfo(String tid) {
+		String sql = "select * from steacher where tid = '" + tid + "';";
 		try {
 			ResultSet rsSet = MySQLConnector.returnConnect(sql);
-			Map<String,String> stuInfo = new HashMap<String,String>();
-			if(rsSet.next()) {
+			Map<String, String> stuInfo = new HashMap<String, String>();
+			if (rsSet.next()) {
 				String ssid = rsSet.getString("tid");
 				stuInfo.put("sid", ssid);
 				String name = rsSet.getString("name");
@@ -26,18 +44,138 @@ public class STeaTools {
 				String major = rsSet.getString("major");
 				stuInfo.put("major", major);
 				return stuInfo;
-			}else {
-				return null;
+			} else {
+				return stuInfo;
 			}
-		}catch (SQLException se) {
-			// TODO: handle exception
-			
-		 	  System.out.println(se);
-		 	  return null;
-		}finally {
-			 // 完成后关闭
-	        MySQLConnector.disconnect();
+		} catch (SQLException se) {
+			System.out.println(se);
+			return null;
+		} finally {
+			// 完成后关闭
+			MySQLConnector.disconnect();
 		}
 	}
+
+	/**
+	 * 给相应教师开已存在的课程
+	 * 
+	 * @param kkid 开课的id
+	 * @param cid  课程的id
+	 * @param tid  老师的id
+	 * @param oid  操作者的id
+	 * @throws TeacherNotExistException 该老师不存在的异常
+	 * @throws CourseNotExistException  该课程不存在的异常
+	 */
+	public static void addSTCourse(String kkid, String cid, String tid,String oid)
+			throws TeacherNotExistException, CourseNotExistException {
+		if (!VerifyTools.isTeacherExist(tid)) {
+			throw new TeacherNotExistException();
+		}
+
+		if (!VerifyTools.isCourseExist(cid)) {
+			throw new CourseNotExistException();
+		}
+
+		String sql = "insert into ct (kkid,cid,tid) values('" + kkid + "','" + cid + "','" + tid + "');";
+		try {
+			MySQLConnector.connect(sql);
+
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			// 完成后关闭
+			MySQLConnector.disconnect();
+		}
+	}
+
+	/**
+	 * 删除相应老师的课
+	 * @param tid  老师的id
+	 * @param cid  课程的id
+	 * @param oid  操作者的id
+	 * @throws CourseNotMatchTeacherException 检测相应老师是否开了此课
+	 */
+	public static void deleteTCourse(String tid, String cid, String oid) throws CourseNotMatchTeacherException {
+
+		if (!VerifyTools.isCourseMatchTeacher(cid, tid)) {
+			throw new CourseNotMatchTeacherException();
+		}
+
+		String sql = "delete from ct where cid = '" + cid + "' and tid = '"+ tid +"';";
+		try {
+			MySQLConnector.connect(sql);
+
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			// 完成后关闭
+			MySQLConnector.disconnect();
+		}
+	}
+	
+	/**
+	 * 查询所有学生成绩
+	 * @param oid  操作者的id
+	 * @return
+	 */
+	public static ArrayList<String[]> searchStuGrade(String oid) {
+		
+		String sql = "select ct.kkid,course.cname,ct.tid,nteacher.tname,sc.sid,stu.name,sc.grade\r\n" + 
+				"from stu,course,nteacher,sc,ct\r\n" + 
+				"where stu.sid=sc.sid and course.cid=ct.cid and nteacher.tid=ct.tid and sc.kkid=ct.kkid ";
+		try {
+			ResultSet rsSet = MySQLConnector.returnConnect(sql);
+			ArrayList<String[]> data = new ArrayList<String[]>();
+			while(rsSet.next()) {
+				String kkid = rsSet.getString("kkid");
+				String cname = rsSet.getString("cname");
+				String tid = rsSet.getString("tid");
+				String tname = rsSet.getString("tname");
+				String sid = rsSet.getString("sid");
+				String sname = rsSet.getString("sname");
+				int grade = rsSet.getInt("grade");
+				String graString = Integer.toString(grade);
+				String[] info = {kkid , cname, tid, tname, graString};
+				data.add(info);
+			}
+			return data;
+		} catch (SQLException se) {
+			// TODO: handle exception
+
+			System.out.println(se);
+			return null;
+		} finally {
+			// 完成后关闭
+			MySQLConnector.disconnect();
+		}
+	}
+	
+	/**
+	 * 修改学生信息
+	 * @param sid  选择修改的学生id
+	 * @param name  学生的姓名
+	 * @param sex  学生的性别
+	 * @param age  学生的年龄
+	 * @param major 学生的专业
+	 * @param oid  操作者的id
+	 * @throws StudentNotExistException  选择的学生id不存在的异常 
+	 */
+	public static void alterStuInfo(String sid, String name, int sex, int age, String major, String oid)throws StudentNotExistException {
+		if (!VerifyTools.isStudentExist(sid)) {
+			throw new StudentNotExistException();
+		}
+		String sql = "upadate stu set name = '" + name + "',sex = " + sex + ",age = " + age + ",major='" + major
+				+ "', where sid = '" + sid + "';";
+		try {
+			MySQLConnector.connect(sql);
+
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			// 完成后关闭
+			MySQLConnector.disconnect();
+		}
+	}
+
 
 }
